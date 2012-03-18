0f950df730ff1a256892a89f953c758d2fa69b4b
Ole Lynge Soerensen <olelynge@gmail.com>
2012-03-18 09:14:38 +0100

    Add comments about psake build scripts.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,5 @@
+"This build script is still incomplete... please use build.cmd and WebTester.build for 'real' builds."
+
+.\GetTools.ps1
 Import-Module .\packages\psake.4.0.1.0\tools\psake.psm1
 .\packages\psake.4.0.1.0\tools\psake.cmd .\scripts\build.ps1
